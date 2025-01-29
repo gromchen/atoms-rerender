30cb647738dcb0437bc4f5bc62a30fd8f04373db
Yuriy Gromchenko <828918+gromchen@users.noreply.github.com>
2025-01-29 19:29:07 +0200

    Add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useRenderInfo } from "@uidotdev/usehooks";
 import { atom, useAtom } from "jotai";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex gap-8 row-start-2 items-center sm:items-start">
         <UseAtomComponent />
       </main>
     </div>
@@ -21,12 +29,18 @@ function UseAtomComponent() {
   const [count, setCount] = useAtom(countAtom);
 
   return (
-    <section>
-      <h1>useAtom</h1>
+    <Card>
+      <CardHeader>
+        <CardTitle>useAtom</CardTitle>
+      </CardHeader>
 
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <CardContent>
+        <code>{JSON.stringify({ count, info }, null, 2)}</code>
+      </CardContent>
 
-      <code>{JSON.stringify({ count, info }, null, 2)}</code>
-    </section>
+      <CardFooter>
+        <Button onClick={() => setCount((prev) => prev + 1)}>+1</Button>
+      </CardFooter>
+    </Card>
   );
 }
